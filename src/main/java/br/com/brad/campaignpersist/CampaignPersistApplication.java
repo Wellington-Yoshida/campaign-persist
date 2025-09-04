94ac6657866b9dc1e19ce9499a475f2dd0df6bda
Wellington Issami Yoshida <issamiyoshida@MacBook-Air-de-Wellington.local>
2025-09-04 20:21:30 -0300

    initial commit

---
+++ src/main/java/br/com/brad/campaignpersist/CampaignPersistApplication.java
@@ -0,0 +1,13 @@
+package br.com.brad.campaignpersist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignPersistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampaignPersistApplication.class, args);
+    }
+
+}
